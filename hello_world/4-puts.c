9dd43e7ed8e7730d248a6000cd111a83f5062c09
AntoineJacob <5646@holbertonstudents.com>
2022-09-30 03:41:28 -0700

    Task 4.1

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 
-int main () {
-char str1[15];
-
-
-strcpy(str1,"Programming is like building a multilingual puzzle");
-
-puts(str1);
-
-   
-return(0);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
