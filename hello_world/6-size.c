846b306c6c105081ed93c5d44c1bef64f6360626
AntoineJacob <5646@holbertonstudents.com>
2022-09-30 04:16:23 -0700

    Task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,11 +5,11 @@ float floatType;
 long longType;
 unsigned long unsignedlongType;
 char charType;
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of long int: %zu bytes\n", sizeof(longType));
-printf("Size of char: %zu byte\n", sizeof(charType));
-printf("Size of long long int: %zu bytes\n", sizeof(unsignedlongType));
+printf("Size of int: %zu byte(s)\n", sizeof(intType));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+printf("Size of long int: %zu byte(s)\n", sizeof(longType));
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of long long int: %zu byte(s)\n", sizeof(unsignedlongType));
 
 return (0);
 }
