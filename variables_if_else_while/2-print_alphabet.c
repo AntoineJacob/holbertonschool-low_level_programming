ef06008a88721fff1c5383ffc95221dd300aedfa
AntoineJacob <5646@holbertonstudents.com>
2022-10-03 06:12:16 -0700

    Task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * Main - We use the function putchar for generate an alphabet in lowercase
+ *
+ * Return : We got alphabet in lower case !
+ */
+
+int main(void)
+{
+  char a[26] = "abcdefghijklmnopqrstuvwxyz";
+  int i;
+   
+  for (i=0 ; i < 26 ; i++)
+    {
+      putchar(a[i]);
+    }
+  putchar('\n');
+  return (0);
+}
+  
