4111d0b9967fb3c17d5164169254525d8965130c
AntoineJacob <5646@holbertonstudents.com>
2022-10-03 05:20:29 -0700

    Task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - Determines either greater than 5, is less than 6, or is 0
+ *main - Determines either greater than 5, is less than 6, or is 0
  *
- * Return Always 0 
+ *Return Always 0 
  */
 int main(void)
 {
