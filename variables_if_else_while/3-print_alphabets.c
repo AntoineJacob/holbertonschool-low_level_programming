3a5dde7a0e22a27f2ee24d0c513390b33fe4c722
AntoineJacob <5646@holbertonstudents.com>
2022-10-03 07:07:36 -0700

    Task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  char a[26] = "abcdefghijklmnopqrstuvwxyz";
-  char b[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  int i;
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-  for (i = 0; i < 26; i++)
-    {
-      putchar(a[i]);
-      putchar(b[i]);
-    }
-  putchar('\n');
-  return (O);
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
