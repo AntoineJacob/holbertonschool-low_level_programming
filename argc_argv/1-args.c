f8e5b331fd970d0150b4153d800663c690f36957
AntoineJacob <5646@holbertonstudents.com>
2022-10-27 01:42:34 -0700

    Task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * Print_arg - Print the number of arg
+ * argc: argc
+ * argv: argv
+ * Return: Return 0.
+ **/
+
+int print_arg(int argc, char *argv[])
+{
+	printf("%d");
+	return (0);
+}
