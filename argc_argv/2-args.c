afa2590249f735e99436a284fdd729f33747822d
AntoineJacob <5646@holbertonstudents.com>
2022-10-27 03:21:15 -0700

    Task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#incude "main.h"
+
+/**
+ * Main - function who return each arg
+ * @argc: argc
+ * @argv: argv
+ * @incr: incr
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int incr;
+
+	if (argc > 0 )
+	{
+		for (incr = 0; incr < 0; incr++)
+		{
+			printf("%s\n", argv[incr]);
+		}
+	}
+	else
+	{
+		printf("\n");
+	}
+	return (0);
+}
