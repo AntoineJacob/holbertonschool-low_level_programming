1d9bcdaaa81b6bdbe782eba7048f728685bbbb7e
AntoineJacob <5646@holbertonstudents.com>
2022-10-31 03:08:16 -0700

    Task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - function
+ * @str: string
+ * Return: NULL or ar.
+ */
+char *_strdup(char *str)
+{
+	int size = 0, i = 0;
+	char *ar;
+
+	if (str == NULL)
+		return (NULL);
+	for (size = 0; str[size] != '\0'; size++)
+		;
+	size++;
+	ar = malloc(size * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		ar[i] = str[i];
+		i++;
+	}
+	return (ar);
+}
