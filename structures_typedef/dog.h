21ea950647e8e0f83cab39a51f92760bb877069f
AntoineJacob <5646@holbertonstudents.com>
2022-11-03 01:42:06 -0700

    Task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ *struct dog - dog structure.
+ *@name: name
+ *@owner: owner.
+ *@age: age
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+/**
+ *dog - new typedef dog
+ */
+typedef struct dog;
