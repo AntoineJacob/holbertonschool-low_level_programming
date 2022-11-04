f0721f79cdc9b19819b1a8d22777ca4d1eaea8bc
vagrant <vagrant@ubuntu2004.localdomain>
2022-11-04 13:20:24 +0000

    Task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+* print_name - Function who print name
+* f: pointer function
+* name: name to putchar
+*Return: 0
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (f = NULL)
+	return (0);
+	(f*)(name);
+}
