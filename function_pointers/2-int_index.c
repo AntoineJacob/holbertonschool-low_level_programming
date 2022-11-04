8330766bccd5760f73130c2b45a59fbc20b5ae29
vagrant <vagrant@ubuntu2004.localdomain>
2022-11-04 14:27:42 +0000

    Task 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function who searches for an integer
+ * @array: the array to check
+ * @size: the size of the array
+ * @cmp: function pointer
+ * Return: -1 / tab
+ */
+
+ int int_index(int *array, int size, int (*cmp)(int))
+ {
+	int tab;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (cmp == NULL || array == NULL)
+	{
+		return (-1)
+	}
+	for (tab = 0; tab < size; tab++)
+	{
+		if (cmp(array[tab]))
+		{
+			return (tab);
+		}
+		return (-1);
+	}
+ }
