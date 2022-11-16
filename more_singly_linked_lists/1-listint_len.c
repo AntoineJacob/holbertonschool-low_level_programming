9f655acdfd4d219615e1926d74ded2cdc6879f66
AntoineJacob <5646@holbertonstudents.com>
2022-11-16 05:55:13 -0800

    Adding the task 1

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *size_t lisint_len - function that print the length of the linked list
+ *@h: head, first node
+ *Return: return the length of the linked list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t number = 0;
+
+	while (h != NULL)
+	{
+		number++;
+		h = h->next;
+	}
+	return(number);
+}
