5dc108b549612d3790f3227b6947e17b9e9100f3
AntoineJacob <5646@holbertonstudents.com>
2022-11-24 04:53:10 -0800

    Fixing betty

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (h == NULL)
 		return (NULL);
 
-	h->array = malloc(sizeof(hash_node_t*) *size);
+	h->array = malloc(sizeof(hash_node_t *) * size);
 	h->size = size;
 
 	return (h);
