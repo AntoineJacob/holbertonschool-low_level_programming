91dc0264ab9292c91a5ba7cb7a7864a10f62880a
AntoineJacob <5646@holbertonstudents.com>
2022-11-24 04:07:54 -0800

    Fixing task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,47 +1,41 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - function that inserts a new node at a position
- * @h: header of the list
- * @idx: position of the new node
- * @n: data of the new node
- * Return: address of the new node
+ * insert_dnodeint_at_index - function that inserts a new node at a given po
+ * @h: head
+ * @idx: index of the list where the new node should be added.
+ * @n: data of new node
+ * Return: the address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *ant, *temp;
-	unsigned int i;
+	dlistint_t *new, *cpy = *h;
+	unsigned int i = 0;
 
-	if (h == NULL)
-		return (NULL);
-
-	new = malloc(sizeof(*h));
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
-	i = 0;
-	if (!*h)
-	{
-		new->next = NULL;
-		*h = new;
-		return (new);
-	}
-	ant = *h;
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	while (i < (idx - 1))
+
+	while (i < idx - 1)
 	{
-		ant = ant->next;
-		i++;
-		if (ant == NULL && i < (idx - 1))
+		cpy = cpy->next;
+		if (cpy == NULL)
 			return (NULL);
+		i++;
 	}
-	if (ant->next == NULL)
-		return (add_dnodeint_end(&ant, n));
-	temp = ant;
-	new->next = temp->next;
-	new->n = n;
-	new->next = new;
+	if (cpy->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+
+	new->prev = cpy;
+	new->next = cpy->next;
+	cpy->next->prev = new;
+	cpy->next = new;
+
 	return (new);
 }
