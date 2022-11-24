b3b08c0f6452389c0603dff82d9a39a1267c0044
AntoineJacob <5646@holbertonstudents.com>
2022-11-24 04:47:03 -0800

    Adding all requirements and task 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hashtable empty
+ * @size: size of the array in the hashtable
+ * Return: new hashtable or null if fails
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *h;
+
+	h = malloc(sizeof(hash_table_t));
+
+	if (h == NULL)
+		return (NULL);
+
+	h->array = malloc(sizeof(hash_node_t*) *size);
+	h->size = size;
+
+	return (h);
+}
