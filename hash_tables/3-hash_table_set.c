637b7084db8813277ceef8938674955975dde3a5
AntoineJacob <5646@holbertonstudents.com>
2022-11-25 06:04:30 -0800

    Fixing task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,45 +1,49 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_set - function that adds an element to the hash table
- * @ht: hash table
- * @key: current key
- * @value: value associated with the key
- * Return: 1 if succeed or 0 if not
+ * @ht: the hash table to add or update the key/value to
+ * @key: the key
+ * @value: the value associated with the key
+ * Return: 1 if success, 0 if failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *pocket, *new;
+	hash_node_t *new;
+	unsigned long int idx, i;
+	char *v;
 
-	if (key == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL)
 		return (0);
 
-	if (ht == NULL)
+	v = strdup(value);
+	if (v == NULL)
 		return (0);
 
-	if (value == NULL)
-		return (0);
-
-	index = key_index((unsigned char *)key, ht->size);
-	pocket = malloc(sizeof(hash_node_t));
-
-	if (pocket == NULL)
-		return (0);
-	pocket->key = strdup(key);
-	pocket->value = strdup(value);
-	pocket->next = ht->array[index];
-	ht->array[index] = pocket;
-	return (1);
-
-	new = ht->array[index];
-	while (new != NULL)
+	idx = key_index((const unsigned char *)key, ht->size);
+	for (i = idx; ht->array[i]; i++)
 	{
-		if (strcmp(new->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(new->value);
-			new->value = strdup(value);
+			free(ht->array[i]->value);
+			ht->array[i]->value = v;
 			return (1);
 		}
-		new = new->next;
 	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = v;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+
+	return (1);
 }
