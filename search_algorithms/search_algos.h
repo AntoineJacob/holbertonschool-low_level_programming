42093b028918cf5d9ea9e53cc57d1f611d46366e
AntoineJacob <5646@holbertonstudents.com>
2023-01-05 06:53:44 -0800

    Task 1

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -39,4 +39,7 @@ typedef struct listint_s
 } listint_t;
 
 int linear_search(int *array, size_t size, int value);
+void print_array(int *array, size_t high, size_t low);
+int b_search(int *array, size_t high, size_t low, int value);
+int binary_search(int *array, size_t size, int value);
 #endif
